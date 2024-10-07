9601253d23edbdc3427d6151a26324479f43882d
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-10-07 13:23:37 -0700

    sandbox content

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('ALAB 308A.3.1')
+console.log('ALAB 308A.3.1'):
+
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./databases.js";
+
+function getUserData(id) {
+    const dbs = {
+      db1: db1,
+      db2: db2,
+      db3: db3
+    };
+  }
+  
